8a5c538dc5612d7fe89c366d3783cb3574c5e0e2
Shashidharak89 <shashidharak334@gmail.com>
2025-09-11 16:28:12 +0530

    Initial update

---
+++ src/models/Work.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const WorkSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    usn: { type: String, required: true },
+    content: { type: String, required: true },
+    subject: { type: String, required: true },
+    files: [{ type: String }], // array of Cloudinary URLs
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Work || mongoose.model("Work", WorkSchema);
