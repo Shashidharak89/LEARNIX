b7c43b1b1eb48217de7ac5706ce82efe1ce80f9f
Shashidharak89 <shashidharak334@gmail.com>
2025-09-14 20:55:59 +0530

    Error fix and navbar to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
 
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
-  const { theme, toggleTheme } = useTheme();
 
   return (
     <>
-    
+    <Navbar/>
     </>
   );
 }
