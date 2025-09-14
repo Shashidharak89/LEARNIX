679dd1d31746937986e413adb9c05765cbcefcb1
Shashidharak89 <shashidharak334@gmail.com>
2025-09-14 20:13:34 +0530

    Profile page setup done

---
+++ src/app/profile/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { Navbar } from "../components/Navbar";
+import UserData from "./UserData";
+
+
+export default function profile() {
+
+    return (
+        <div >
+            <Navbar />
+            <UserData />
+        </div>
+    );
+}
