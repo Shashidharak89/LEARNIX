38790655fd990be58ebfe6e9186a191d83234956
Shashidharak89 <shashidharak334@gmail.com>
2025-09-14 19:54:24 +0530

    Search route setup

---
+++ src/app/search/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { Navbar } from "../components/Navbar";
+import UserDetails from "./UserDetails";
+
+export default function search() {
+
+    return (
+        <div >
+            <Navbar />
+            <UserDetails/>
+        </div>
+    );
+}
--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -3,10 +3,6 @@
 import AllUsersRecords from "./AllUsersRecords";
 import ManageSubjects from "./ManageSubjects";
 import { Navbar } from "../components/Navbar";
-import UserDetails from "./UserDetails";
-
-
-
 
 export default function upload() {
 
@@ -15,8 +11,6 @@ export default function upload() {
       <Navbar />
       <ManageSubjects />
       <AllUsersRecords />
-      <UserDetails />
-
     </div>
   );
 }
