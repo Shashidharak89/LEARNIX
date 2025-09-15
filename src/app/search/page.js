120e0f0ec9606eff387a01001b48ffd2acf9e623
Shashidharak89 <shashidharak334@gmail.com>
2025-09-15 19:30:57 +0530

    navbar changes

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -9,7 +9,7 @@ export default function search() {
     return (
         <div >
             <Navbar />
-         <WorkSearchInterface/>
+            <WorkSearchInterface />
         </div>
     );
 }
