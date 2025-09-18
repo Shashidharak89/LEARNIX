cf92c11a0b25f9cce508bd709b650279ff952e5d
Shashidhara K <shashidharak334@gmail.com>
2025-09-18 16:58:34 +0000

    Titke added - Learnix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,34 @@
+// app/layout.js
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
+
+export const metadata = {
+  title: "Learnix",
+  description: "Learnix is a learning platform where students can share their homework, explore solutions, and make learning referable and collaborative.",
+  keywords: [
+    "Learnix", 
+    "student homework", 
+    "learning platform", 
+    "share homework", 
+    "study resources", 
+    "online learning"
+  ],
+  authors: [{ name: "Shashidhara K." }],
+  openGraph: {
+    title: "Learnix - Share and Explore Student Homework",
+    description: "Collaborative platform for students to share homework and learning resources.",
+    url: "https://learnix.shashi-k.in",
+    siteName: "Learnix",
+    type: "website",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Learnix - Share and Explore Student Homework",
+    description: "Collaborative platform for students to share homework and learning resources.",
+    creator: "@YourTwitterHandle", // optional
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
