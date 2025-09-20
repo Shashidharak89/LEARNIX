e56654332fe49c54bcf2f69c3f6a238968c52ce1
Shashidhara K <shashidharak334@gmail.com>
2025-09-20 05:53:00 +0000

    Study meterials upload

--- src/app/materials/materialsData.js
+++ src/app/materials/materialsData.js
@@ -1,49 +1,49 @@
 // src/data/materialsData.js
 
 const materialsData = [
-  {
-    semester: "1st Sem - MCA",
-    subjects: [
-      {
-        subject: "Computer Organization",
-        files: [
-          { name: "Introduction Notes", url: "/files/comp-org-intro.pdf" },
-          { name: "CPU Architecture", url: "/files/cpu-architecture.pdf" },
+    {
+        semester: "1st Sem - MCA",
+        subjects: [
+            {
+                subject: "Mathematics",
+                files: [
+                    { name: "Mathematics Textbook", url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Mathematics/MATHS-TEXT%20BOOK.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vTWF0aGVtYXRpY3MvTUFUSFMtVEVYVCBCT09LLnBkZiIsImlhdCI6MTc1ODM0Njg1OCwiZXhwIjo0OTExOTQ2ODU4fQ.tqCoCnOVL7kaCeGRvUAYMdLtOQljb51LxbEgVHQCEM4" },
+                ],
+            },
+            {
+                subject: "Data Communication and Networking",
+                files: [
+                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/Data%20Communication%20and%20Networks-Introduction.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EYXRhIENvbW11bmljYXRpb24gYW5kIE5ldHdvcmtzLUludHJvZHVjdGlvbi5wZGYiLCJpYXQiOjE3NTgzNDcxMDksImV4cCI6MTc4OTg4MzEwOX0.UVwCjh0s2OxkfwM4H-S6z0tmkigrAUe0idJvmq7nY5I" },
+                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-Data%20Encoding.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tRGF0YSBFbmNvZGluZy5wZGYiLCJpYXQiOjE3NTgzNDcxNDgsImV4cCI6MTc4OTg4MzE0OH0.651JXQtIz9yhODYGOIzII07FDD5SCooGLVrl9wLZ1N4" },
+                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-protocol%20architecture.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tcHJvdG9jb2wgYXJjaGl0ZWN0dXJlLnBkZiIsImlhdCI6MTc1ODM0NzE3NCwiZXhwIjoxNzg5ODgzMTc0fQ.6mVLUrpI4296qBjYWVpwWT0frc5P4A1Z86jygl_HkJo"},
+                ],
+            },
+            {
+                subject: "Research Methodology and Publication Ethics",
+                files: [
+                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U1.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMS5wcHR4IiwiaWF0IjoxNzU4MzQ3MzMzLCJleHAiOjE3ODk4ODMzMzN9.lKCNckEILpEt705szak51QgAKdsKOYjC6lVb3nWzSA0"},
+                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U2.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMi5wcHR4IiwiaWF0IjoxNzU4MzQ3MzY4LCJleHAiOjE3ODk4ODMzNjh9.1kKXo2ApNK4gIp5L_3a8QVBtXE_56fjdy1LR97HzGds"},
+                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U3.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMy5wcHR4IiwiaWF0IjoxNzU4MzQ3MzgwLCJleHAiOjE3ODk4ODMzODB9.YXILKTNK1llipJJ6nmHGyYYfMBz4GRLScrOu8aZvjhs"},
+                ],
+            },
         ],
-      },
-      {
-        subject: "Programming in C",
-        files: [
-          { name: "C Basics", url: "/files/c-basics.pdf" },
-          {url: "/files/pointers.pdf" },
+    },
+    {
+        semester: "2nd Sem - MCA",
+        subjects: [
+            {
+                
+            },
         ],
-      },
-    ],
-  },
-  {
-    semester: "2nd Sem - MCA",
-    subjects: [
-      {
-        subject: "Data Structures",
-        files: [
-          { name: "Stacks & Queues", url: "/files/stacks-queues.pdf" },
-          { name: "Trees", url: "/files/trees.pdf" },
+    },
+    {
+        semester: "3rd Sem - MCA",
+        subjects: [
+            {
+                
+            },
         ],
-      },
-    ],
-  },
-  {
-    semester: "3rd Sem - MCA",
-    subjects: [
-      {
-        subject: "Database Systems",
-        files: [
-          { name: "SQL Basics", url: "/files/sql-basics.pdf" },
-          { name: "Normalization", url: "/files/normalization.pdf" },
-        ],
-      },
-    ],
-  },
+    },
 ];
 
 export default materialsData;
