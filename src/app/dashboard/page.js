424bc802689c0255331bfca35df5d248accf0894
Shashidharak89 <shashidharak334@gmail.com>
2025-09-23 22:19:15 +0530

    testing commit

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -6,8 +6,6 @@ import WorkSearchInterface from "../search/WorkSearchInterface";
 import AllUsersRecords from "./AllUsersRecords";
 import DashBoard from "./DashBoard";
 
-
-
 export default function dashboard() {
 
   return (
