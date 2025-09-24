bdd472dcff54a4e843aa82bc16c00c00eb7fa868
Shashidharak89 <shashidharak334@gmail.com>
2025-09-25 00:32:20 +0530

    Materials update

--- src/app/materials/materialsData.js
+++ src/app/materials/materialsData.js
@@ -4,10 +4,17 @@ const materialsData = [
     {
         semester: "1st Sem - MCA",
         subjects: [
+                {
+                subject: "Syllabus",
+                files: [
+                    {name:"Syllabus", url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Syllabus/1stMCA-Syllabus.pdf"},
+                ],
+            },
             {
                 subject: "Mathematics",
                 files: [
                     { name: "Mathematics Textbook", url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Mathematics/MATHS-TEXT%20BOOK.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vTWF0aGVtYXRpY3MvTUFUSFMtVEVYVCBCT09LLnBkZiIsImlhdCI6MTc1ODM0Njg1OCwiZXhwIjo0OTExOTQ2ODU4fQ.tqCoCnOVL7kaCeGRvUAYMdLtOQljb51LxbEgVHQCEM4" },
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Mathematics/VassXMSE1SEM1%262.pdf" },
                 ],
             },
             {
@@ -15,24 +22,69 @@ const materialsData = [
                 files: [
                     { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/Data%20Communication%20and%20Networks-Introduction.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EYXRhIENvbW11bmljYXRpb24gYW5kIE5ldHdvcmtzLUludHJvZHVjdGlvbi5wZGYiLCJpYXQiOjE3NTgzNDcxMDksImV4cCI6MTc4OTg4MzEwOX0.UVwCjh0s2OxkfwM4H-S6z0tmkigrAUe0idJvmq7nY5I" },
                     { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-Data%20Encoding.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tRGF0YSBFbmNvZGluZy5wZGYiLCJpYXQiOjE3NTgzNDcxNDgsImV4cCI6MTc4OTg4MzE0OH0.651JXQtIz9yhODYGOIzII07FDD5SCooGLVrl9wLZ1N4" },
-                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-protocol%20architecture.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tcHJvdG9jb2wgYXJjaGl0ZWN0dXJlLnBkZiIsImlhdCI6MTc1ODM0NzE3NCwiZXhwIjoxNzg5ODgzMTc0fQ.6mVLUrpI4296qBjYWVpwWT0frc5P4A1Z86jygl_HkJo"},
+                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-protocol%20architecture.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tcHJvdG9jb2wgYXJjaGl0ZWN0dXJlLnBkZiIsImlhdCI6MTc1ODM0NzE3NCwiZXhwIjoxNzg5ODgzMTc0fQ.6mVLUrpI4296qBjYWVpwWT0frc5P4A1Z86jygl_HkJo" },
+                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-And-Computer-Communication/unit2-ch1.pdf" },
                 ],
             },
             {
                 subject: "Research Methodology and Publication Ethics",
                 files: [
-                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U1.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMS5wcHR4IiwiaWF0IjoxNzU4MzQ3MzMzLCJleHAiOjE3ODk4ODMzMzN9.lKCNckEILpEt705szak51QgAKdsKOYjC6lVb3nWzSA0"},
-                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U2.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMi5wcHR4IiwiaWF0IjoxNzU4MzQ3MzY4LCJleHAiOjE3ODk4ODMzNjh9.1kKXo2ApNK4gIp5L_3a8QVBtXE_56fjdy1LR97HzGds"},
-                    {url:"https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U3.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMy5wcHR4IiwiaWF0IjoxNzU4MzQ3MzgwLCJleHAiOjE3ODk4ODMzODB9.YXILKTNK1llipJJ6nmHGyYYfMBz4GRLScrOu8aZvjhs"},
+                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U1.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMS5wcHR4IiwiaWF0IjoxNzU4MzQ3MzMzLCJleHAiOjE3ODk4ODMzMzN9.lKCNckEILpEt705szak51QgAKdsKOYjC6lVb3nWzSA0" },
+                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U2.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMi5wcHR4IiwiaWF0IjoxNzU4MzQ3MzY4LCJleHAiOjE3ODk4ODMzNjh9.1kKXo2ApNK4gIp5L_3a8QVBtXE_56fjdy1LR97HzGds" },
+                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Research_Methodology_And_Publication_Ethics/RM_U3.pptx?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vUmVzZWFyY2hfTWV0aG9kb2xvZ3lfQW5kX1B1YmxpY2F0aW9uX0V0aGljcy9STV9VMy5wcHR4IiwiaWF0IjoxNzU4MzQ3MzgwLCJleHAiOjE3ODk4ODMzODB9.YXILKTNK1llipJJ6nmHGyYYfMBz4GRLScrOu8aZvjhs" },
                 ],
             },
+            {
+                subject: "Advanced Data Base Systems",
+                files: [
+                    { name: "ADBS Textbook", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/refs/heads/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS-Text-Book.pdf" },
+                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS_Unit%201_Intro%20to%20DB.pdf" },
+                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS_Unit%202_SQL.pdf" },
+                ],
+            },
+            {
+                subject: "Advanced Operating System with Unix",
+                files: [
+                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/AOS%20ch2.pptx" },
+                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study\_Materials\_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/Aos%20ch1.pptx](https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/Aos%20ch1.pptx" },
+                ],
+            },
+               {
+                subject: "Data Structures And Algorithms",
+                files: [
+                    
+                    {name:"UNIT-1--CPT1",url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Arrays_in_C_Presentation.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20simple.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20using%20array%20of%20objects.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20using%20nested%20structure%20concept.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C_Functions_Presentation.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Introduction%20to%20DS-edited.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Introduction_to_DSA.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Pointers.pdf"},
+                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/structures-edited.pdf"},
+                    {name:"UNIT-1--CPT2-1", url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%202/Design%20and%20Analysis%20of%20Algorithms.pdf"},
+                    {url:"https://github.com/Shashidharak89/Study_Materials_Collection/blob/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Doubly%20Linked%20Lists.pdf"},
+                    {url:"https://github.com/Shashidharak89/Study_Materials_Collection/blob/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Realization%20of%20DLL%20program.pdf"},
+                    {url:"https://github.com/Shashidharak89/Study_Materials_Collection/blob/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Realization%20of%20Singly%20Linked%20List-edited.pdf"},
+                    {url:"https://github.com/Shashidharak89/Study_Materials_Collection/blob/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/realization%20of%20SLL%20program.pdf"},
+                    
+                ],
+            },
+            //    {
+            //     subject: "Research Methodology and Publication Ethics",
+            //     files: [
+            //         {url:""},
+            //         {url:""},
+            //         {url:""},
+            //     ],
+            // },
         ],
     },
     {
         semester: "2nd Sem - MCA",
         subjects: [
             {
-                
+
             },
         ],
     },
@@ -40,7 +92,7 @@ const materialsData = [
         semester: "3rd Sem - MCA",
         subjects: [
             {
-                
+
             },
         ],
     },
