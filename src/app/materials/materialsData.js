888eddc3160bc8804bfeea6084959bc72c178794
Shashidharak89 <shashidharak334@gmail.com>
2025-10-02 23:26:56 +0530

    Names update on Matereals data

--- src/app/materials/materialsData.js
+++ src/app/materials/materialsData.js
@@ -4,17 +4,17 @@ const materialsData = [
     {
         semester: "1st Sem - MCA",
         subjects: [
-                {
+            {
                 subject: "Syllabus",
                 files: [
-                    {name:"Syllabus", url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Syllabus/1stMCA-Syllabus.pdf"},
+                    { name: "Syllabus", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Syllabus/1stMCA-Syllabus.pdf" },
                 ],
             },
             {
                 subject: "Mathematics",
                 files: [
                     { name: "Mathematics Textbook", url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Mathematics/MATHS-TEXT%20BOOK.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vTWF0aGVtYXRpY3MvTUFUSFMtVEVYVCBCT09LLnBkZiIsImlhdCI6MTc1ODM0Njg1OCwiZXhwIjo0OTExOTQ2ODU4fQ.tqCoCnOVL7kaCeGRvUAYMdLtOQljb51LxbEgVHQCEM4" },
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Mathematics/VassXMSE1SEM1%262.pdf" },
+                    { name: "VassXMSE1SEM1%262.pdf Handwritten Notes", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Mathematics/VassXMSE1SEM1%262.pdf" },
                 ],
             },
             {
@@ -38,37 +38,37 @@ const materialsData = [
                 subject: "Advanced Data Base Systems",
                 files: [
                     { name: "ADBS Textbook", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/refs/heads/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS-Text-Book.pdf" },
-                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS_Unit%201_Intro%20to%20DB.pdf" },
-                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS_Unit%202_SQL.pdf" },
+                    { name: "ADBS Unit-1", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS_Unit%201_Intro%20to%20DB.pdf" },
+                    { name: "ADBS Unit-2", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Data-Base-Systems/ADBS_Unit%202_SQL.pdf" },
                 ],
             },
             {
                 subject: "Advanced Operating System with Unix",
                 files: [
-                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/AOS%20ch2.pptx" },
-                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study\_Materials\_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/Aos%20ch1.pptx](https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/Aos%20ch1.pptx" },
+                    { name: "AOS Ch2", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/AOS%20ch2.pptx" },
+                    { name: "AOS ch1", url: "https://raw.githubusercontent.com/Shashidharak89/Study\_Materials\_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/Aos%20ch1.pptx](https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Advanced-Operating-System-with-Unix/Aos%20ch1.pptx" },
                 ],
             },
-               {
+            {
                 subject: "Data Structures And Algorithms",
                 files: [
-                    
-                    {name:"UNIT-1--CPT1",url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Arrays_in_C_Presentation.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20simple.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20using%20array%20of%20objects.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20using%20nested%20structure%20concept.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C_Functions_Presentation.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Introduction%20to%20DS-edited.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Introduction_to_DSA.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Pointers.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/structures-edited.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Abstract_Data_Types.pdf"},
-                    {name:"UNIT-1--CPT2-1", url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%202/Design%20and%20Analysis%20of%20Algorithms.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Doubly%20Linked%20Lists.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Realization%20of%20DLL%20program.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Realization%20of%20Singly%20Linked%20List-edited.pdf"},
-                    {url:"https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/realization%20of%20SLL%20program.pdf"},
-                    
+
+                    { name: "UNIT-1--CPT1", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Arrays_in_C_Presentation.pdf" },
+                    { name: "C Structure program", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20simple.pdf" },
+                    { name: "C Structure program using array", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20using%20array%20of%20objects.pdf" },
+                    { name: "C Structure program using nested Structure", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C%20structure%20Program%20using%20nested%20structure%20concept.pdf" },
+                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/C_Functions_Presentation.pdf" },
+                    { name: "Introduction to DS", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Introduction%20to%20DS-edited.pdf" },
+                    { name: "Introduction to DSA", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Introduction_to_DSA.pdf" },
+                    { name: "Pointers", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Pointers.pdf" },
+                    { name: "Structures Edited", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/structures-edited.pdf" },
+                    { name: "Abstract Data Types", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%201/Abstract_Data_Types.pdf" },
+                    { name: "Design and Analysis of Algorithms U1-CP1", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unite%20I/Chapter%202/Design%20and%20Analysis%20of%20Algorithms.pdf" },
+                    { name: "Doubly Linked lists", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Doubly%20Linked%20Lists.pdf" },
+                    { name: "Realization of DLL", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Realization%20of%20DLL%20program.pdf" },
+                    { name: "Realization if Singly Linked List", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/Realization%20of%20Singly%20Linked%20List-edited.pdf" },
+                    { name: "Realization of SLL Program", url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-Structures-And-Algorithms/Unit%20II/realization%20of%20SLL%20program.pdf" },
+
                 ],
             },
             //    {
