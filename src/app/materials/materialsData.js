9a84dca7b4100d871771bde1c3b9c9f9c705e8e3
Shashidharak89 <shashidharak334@gmail.com>
2025-10-30 20:05:59 +0530

    dcn update in study matereals

--- src/app/materials/materialsData.js
+++ src/app/materials/materialsData.js
@@ -20,10 +20,10 @@ const materialsData = [
             {
                 subject: "Data Communication and Networking",
                 files: [
-                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/Data%20Communication%20and%20Networks-Introduction.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EYXRhIENvbW11bmljYXRpb24gYW5kIE5ldHdvcmtzLUludHJvZHVjdGlvbi5wZGYiLCJpYXQiOjE3NTgzNDcxMDksImV4cCI6MTc4OTg4MzEwOX0.UVwCjh0s2OxkfwM4H-S6z0tmkigrAUe0idJvmq7nY5I" },
-                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-Data%20Encoding.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tRGF0YSBFbmNvZGluZy5wZGYiLCJpYXQiOjE3NTgzNDcxNDgsImV4cCI6MTc4OTg4MzE0OH0.651JXQtIz9yhODYGOIzII07FDD5SCooGLVrl9wLZ1N4" },
-                    { url: "https://fdaiokzqlgiwevwfqbfy.supabase.co/storage/v1/object/sign/Study-Metereals/1-sem/Data_Communication_And_Networks/DCN-protocol%20architecture.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV81NTU3NmU1OS0zMWY3LTQ3YTktODE5NC05MmU3YmNhODliNmUiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJTdHVkeS1NZXRlcmVhbHMvMS1zZW0vRGF0YV9Db21tdW5pY2F0aW9uX0FuZF9OZXR3b3Jrcy9EQ04tcHJvdG9jb2wgYXJjaGl0ZWN0dXJlLnBkZiIsImlhdCI6MTc1ODM0NzE3NCwiZXhwIjoxNzg5ODgzMTc0fQ.6mVLUrpI4296qBjYWVpwWT0frc5P4A1Z86jygl_HkJo" },
-                    { url: "https://raw.githubusercontent.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-And-Computer-Communication/unit2-ch1.pdf" },
+                    { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-And-Computer-Communication/Unit1/unit2-ch1.pdf" },
+                    { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-And-Computer-Communication/Unit1/App%20Layer-Unit2-Ch%202.pdf" },
+                    { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-And-Computer-Communication/Unit2/App%20Layer-Unit2-Ch%202.pdf" },
+                    { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/1st-Sem/Data-And-Computer-Communication/Unit2/unit2-ch1.pdf" },
                 ],
             },
             {
@@ -100,11 +100,11 @@ const materialsData = [
     {
         semester: "Other Resources",
         subjects: [
-            { 
+            {
                 subject: "Advanced Operating System with Unix",
                 files: [
-                    {  url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/OTHER_RESOURCES/Operating_System/OPERATING-SYSTEM.docx" },
-                    {  url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/OTHER_RESOURCES/Operating_System/OPERATING-SYSTEM.pdf" },
+                    { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/OTHER_RESOURCES/Operating_System/OPERATING-SYSTEM.docx" },
+                    { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/OTHER_RESOURCES/Operating_System/OPERATING-SYSTEM.pdf" },
                 ],
 
             },
