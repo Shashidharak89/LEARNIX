a22a5dffc1d1ccf90e1c1e5cc4103312c65dd14e
Shashidhara K <shashidharak334@gmail.com>
2025-12-02 22:00:46 +0530

    ads snippet

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,11 @@ export const metadata = {
   title: "Learnix",
   description: "Learnix is a learning platform where students can share their homework, explore solutions, and make learning referable and collaborative.",
   keywords: [
-    "Learnix", 
-    "student homework", 
-    "learning platform", 
-    "share homework", 
-    "study resources", 
+    "Learnix",
+    "student homework",
+    "learning platform",
+    "share homework",
+    "study resources",
     "online learning"
   ],
   authors: [{ name: "Shashidhara K." }],
@@ -36,6 +36,8 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-adsense-account" content="ca-pub-4934238485595915" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4934238485595915" crossOrigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4934238485595915"
+          crossorigin="shashidhara k"></script>
       </head>
       <body>
         <ThemeProvider>
