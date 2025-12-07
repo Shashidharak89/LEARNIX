f0a0b70a148528c5a4ebb91ace053945e4dd27c2
Shashidharak89 <shashidharak334@gmail.com>
2025-12-07 13:38:07 +0530

    get profile by USN working code added

--- src/app/search/[usn]/page.js
+++ src/app/search/[usn]/page.js
@@ -2,8 +2,8 @@ import { Navbar } from "@/app/components/Navbar";
 import UserDetailsPage from "./UserDetailsPage";
 import Footer from "@/app/components/Footer";
 
-export default function Page({ params }) {
-  const { usn } = params||"";
+export default async function Page({ params }) {
+  const { usn } = await params;
 
   return (
     <div>
