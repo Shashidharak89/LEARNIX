5a6b758db961a4c21a7036815e139a5a3e1c8e7a
Shashidharak89 <shashidharak334@gmail.com>
2026-01-30 10:02:45 +0530

    Popunder ads script added in / page

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,11 @@ export default function Home() {
     <>
     <Navbar/>
     <HeroSection/>
+    {/* Monetization Popunder Script for zone #6760674 */}
+    <script
+      src="https://wellmade-combine.com/bx3PV-0RP.3mpVvLbMmkVqJMZ_Du0x2SN-ziYbwCNfjocj0/LxTbYD3WNrjAAH2cN/zHUo"
+      async
+    ></script>
     </>
   );
 }
