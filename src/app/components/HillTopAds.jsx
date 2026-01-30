3bc58b502a254e06aebf8884b09c19e0532dcfd4
Shashidharak89 <shashidharak334@gmail.com>
2026-01-30 10:06:44 +0530

    HillTopAds component created

---
+++ src/app/components/HillTopAds.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+
+const HillTopAds = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://wellmade-combine.com/bx3PV-0RP.3mpVvLbMmkVqJMZ_Du0x2SN-ziYbwCNfjocj0/LxTbYD3WNrjAAH2cN/zHUo";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div style={{ width: "100%", textAlign: "center", margin: "24px 0" }}>
+      {/* HilltopAds Popunder will be triggered by script */}
+      <span style={{ fontSize: "0.9em", color: "#888" }}>Advertisement</span>
+    </div>
+  );
+};
+
+export default HillTopAds;
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
+import HillTopAds from "./components/HillTopAds";
 
 export default function Home() {
 
@@ -10,11 +11,7 @@ export default function Home() {
     <>
     <Navbar/>
     <HeroSection/>
-    {/* Monetization Popunder Script for zone #6760674 */}
-    <script
-      src="https://wellmade-combine.com/bx3PV-0RP.3mpVvLbMmkVqJMZ_Du0x2SN-ziYbwCNfjocj0/LxTbYD3WNrjAAH2cN/zHUo"
-      async
-    ></script>
+    <HillTopAds />
     </>
   );
 }
