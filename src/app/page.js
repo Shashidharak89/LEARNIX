4fd4243d1feffeb7acc9240645698a16daf7842c
Shashidharak89 <shashidharak334@gmail.com>
2026-02-01 10:17:11 +0530

    Removed ads from Home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,6 @@
 
 import HeroSection from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
-import HillTopAds from "./components/HillTopAds";
-import VideoAdEmbed from "./components/VideoAdEmbed";
-import VastVideoPlayer from "./components/VastVideoPlayer";
-import VideoSliderMultiTag from "./components/VideoSliderMultiTag";
 
 export default function Home() {
 
@@ -13,8 +9,6 @@ export default function Home() {
     <>
     <Navbar/>
     <HeroSection/>
-    <HillTopAds />
-    <VastVideoPlayer />
     </>
   );
 }
