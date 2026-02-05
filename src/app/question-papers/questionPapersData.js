2a7435548d7d971b840782ddf4b132cfc43e2569
Shashidharak89 <shashidharak334@gmail.com>
2026-02-05 22:52:02 +0530

    Question paper data added

--- src/app/question-papers/questionPapersData.js
+++ src/app/question-papers/questionPapersData.js
@@ -39,12 +39,15 @@ const questionPapersData =
                     {
                         id: "QP0103",
                         imageurls: [
-                            "https://res.cloudinary.com/ddycnd409/image/upload/v1759636097/users/NU25MCA117/Research%20Methodology%20and%20Publication%20Ethics/Class%20notes%20RMPE/gonzhatbvbpnkdah1wqv.jpg",
-                            "https://res.cloudinary.com/ddycnd409/image/upload/v1759636097/users/NU25MCA117/Research%20Methodology%20and%20Publication%20Ethics/Class%20notes%20RMPE/gonzhatbvbpnkdah1wqv.jpg",
+                            "https://res.cloudinary.com/ddycnd409/image/upload/v1766394145/users/NN25MCA119/Previous%20year%20Question%20papers/1-Sem%20Exam%20QP%20%5B2022-24%5D/cevfvymouzafh76aedqh.jpg",
+                            "https://res.cloudinary.com/ddycnd409/image/upload/v1766394206/users/NN25MCA119/Previous%20year%20Question%20papers/1-Sem%20Exam%20QP%20%5B2022-24%5D/o6ohsihocrfttggtxkjc.jpg",
+                            "https://res.cloudinary.com/ddycnd409/image/upload/v1766394388/users/NN25MCA119/Previous%20year%20Question%20papers/1-Sem%20Exam%20QP%20%5B2022-24%5D/gocrxbknhcvomxxacfhm.jpg",
+                            "https://res.cloudinary.com/ddycnd409/image/upload/v1766394687/users/NN25MCA119/Previous%20year%20Question%20papers/1-Sem%20Exam%20QP%20%5B2022-24%5D/ekbs7gfdwopcbulnmkma.jpg",
+                            "https://res.cloudinary.com/ddycnd409/image/upload/v1766394790/users/NN25MCA119/Previous%20year%20Question%20papers/1-Sem%20Exam%20QP%20%5B2022-24%5D/ovrvswrot6t8v8chjpde.jpg",
+                            "https://res.cloudinary.com/ddycnd409/image/upload/v1766394804/users/NN25MCA119/Previous%20year%20Question%20papers/1-Sem%20Exam%20QP%20%5B2022-24%5D/ju47soq5aryotgnuk4kc.jpg"
                         ],
                         visitlink: [
-                            "6984a0ca838d91b67e95a1c7",
-                            "6984a0ca838d91b67e95a1c7"
+                            "694908f49095ea0a29bf5b33"
                         ]
 
                     },
