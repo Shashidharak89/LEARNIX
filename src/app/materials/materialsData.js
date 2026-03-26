5304e43ae2f85b05a34ff97bfd61af21b34727ba
Shashidhara K <shashidharak334@gmail.com>
2026-03-26 07:53:11 +0530

    AWT A4 added

--- src/app/materials/materialsData.js
+++ src/app/materials/materialsData.js
@@ -220,7 +220,10 @@ const materialsData = [
                     { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/2nd-Sem-2025-26/Advanced-Web-Technologies-Lab/Assignment%203.docx" },
                     { url: "https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/2nd-Sem-2025-26/Advanced-Web-Technologies-Lab/Assignment%204.docx" },
 
-                ]
+                ],
+                customfiles: [
+                    {url:"https://raw.github.com/Shashidharak89/Study_Materials_Collection/main/MCA-NMAMIT/2nd-Sem-2025-26/Advanced-Web-Technologies-Lab/External/AWT-Assignment4-Solution.pdf"},
+                ],
 
             },
             {
