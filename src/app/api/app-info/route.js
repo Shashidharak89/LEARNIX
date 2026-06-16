49be7209ab70f53247483d4b5c24c72b01327b82
Shashidhara K <shashidharak334@gmail.com>
2026-06-16 17:30:42 +0530

    Refactor : Version api updated with latest version

--- src/app/api/app-info/route.js
+++ src/app/api/app-info/route.js
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const version = "1.2.7";
-  const link = "https://play.google.com/store/apps/details?id=com.shashidharak.learnix";
+  const latestVersion = "1.3.4";
+  const validVersion = "1.2.0";
+  const appLink = "https://play.google.com/store/apps/details?id=com.shashidharak.learnix";
 
   return NextResponse.json({
-    version,
-    link
+    version: latestVersion, // For backward compatibility
+    link: appLink, // For backward compatibility
+    latestVersion,
+    validVersion,
+    appLink
   });
 }
