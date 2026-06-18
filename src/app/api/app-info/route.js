e2aa87641dd436b386e9ffbbdb9212c8740d7e6d
Shashidhara K <shashidharak334@gmail.com>
2026-06-18 12:06:32 +0530

    Update : New update of v1.3.5 available

--- src/app/api/app-info/route.js
+++ src/app/api/app-info/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const latestVersion = "1.3.4";
-  const validVersion = "1.2.0";
+  const latestVersion = "1.3.5";
+  const validVersion = "1.3.4";
   const appLink = "https://play.google.com/store/apps/details?id=com.shashidharak.learnix";
 
   return NextResponse.json({
