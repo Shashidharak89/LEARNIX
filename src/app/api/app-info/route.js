bcc23b07ca5135e10a94565fdff1770371038d9e
Shashidhara K <shashidharak334@gmail.com>
2026-07-06 17:28:43 +0530

    Feat : New version added v1.3.11

--- src/app/api/app-info/route.js
+++ src/app/api/app-info/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const latestVersion = "1.3.10";
-  const validVersion = "1.3.4";
+  const latestVersion = "1.3.11";
+  const validVersion = "1.3.11";
   const appLink = "https://play.google.com/store/apps/details?id=com.shashidharak.learnix";
 
   return NextResponse.json({
